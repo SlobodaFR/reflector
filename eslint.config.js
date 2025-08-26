ec8b1e761c5d3ef8fffe637977f7df9d997893a3
Thomas SLOBODA <thomas@sloboda.fr>
2025-08-26 17:48:37 +0200

    chore(tooling): migrate eslint config to v9 format, ignore commitlint config, add full CI tooling

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['commitlint.config.js'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
