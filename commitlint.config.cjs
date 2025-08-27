16a53f147b890168af53e25daeef603c532159f9
Thomas Sloboda <thomas@sloboda.fr>
2025-08-27 08:07:57 +0200

    fix(commitlint): allow release type and unlimited body line length for semantic-release (#3)

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [0, 'always', 1000]
+        'type-enum': [2, 'always', [
+            'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'release'
+        ]],
+        'header-max-length': [0, 'always', 1000],
+        'body-max-line-length': [0, 'always', Infinity]
     }
 };
