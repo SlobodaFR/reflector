816ac977291249caba7cdd307d038cf78036a38d
Thomas SLOBODA <thomas@sloboda.fr>
2025-08-27 08:11:36 +0200

    fix(commitlint): disable footer max line length for semantic-release

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
             'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'release'
         ]],
         'header-max-length': [0, 'always', 1000],
-        'body-max-line-length': [0, 'always', Infinity]
+        'body-max-line-length': [0, 'always', Infinity],
+        'footer-max-line-length': [0, 'always', Infinity]
     }
 };
